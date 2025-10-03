1258d484aa6cbbc7f9dabd2a10685076eb1a8434
Shingo Hosotani <hosotani.shingo@uhuru.jp>
2025-10-03 12:37:41 +0900

    Setting mysql

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'test_database'),
+        'USER': os.environ.get('MYSQL_USER', 'docker'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'docker'),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
