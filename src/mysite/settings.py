bfb7d26db2644a4193bfd33c0e05db18fbd9e0e9
Shingo Hosotani <hosotani.shingo@uhuru.jp>
2025-10-03 14:27:39 +0900

    setting eb

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-xitox7-=ids26yzc#nzbd4o+ap_32@6s&ln+hq=z=(g0ni$+^j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+if "IS_AWS" in os.environ:
+    ALLOWED_HOSTS = ["eb-tutorial-dev.ap-northeast-1.elasticbeanstalk.com"]
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
